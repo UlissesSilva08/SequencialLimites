b1b497e87a6d98467a71e27ac5dce05649a1ac7c
UlissesSilva08 <ulissesmasterchief@gmail.com>
2021-11-27 23:26:05 -0300

    Inserindo as linha de código no programa

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SequencialLimites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inicio: ");
+            int inicio = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Fim: ");
+            int fim = Convert.ToInt32(Console.ReadLine());
+
+            if (fim > inicio)
+            {
+                int n = inicio;
+
+                while (n < fim)
+                {
+                     Console.Write($"{n} ");
+                     n += 1;
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Não é possibvel terminar anterior ao início.");
+            }
+        }
+    }
+}
